e26f3dfa2543927c84bef05b14664605d24660a5
shmulik55 <gshloman@gmail.com>
2024-09-22 19:44:34 +0300

    Started welcome section

---
+++ src/Components/WelcomeSection.jsx
@@ -0,0 +1,12 @@
+
+function Welcome(){
+    return (
+        <section id="welcome-section">
+            <h1 id="developer">Hey, I am Guy.</h1>
+            <span id="developer-title">front-end-developer.</span>
+            <p>I'm currently a high school student, studying physics and system engineering. I try to focus on learning front end as much as I can and do my best.</p>
+        </section>
+    );
+}
+
+export default Welcome;
