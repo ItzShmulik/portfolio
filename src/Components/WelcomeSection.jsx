d3954dcfead5902d263a63190a7e049bb59270ea
shmulik55 <gshloman@gmail.com>
2024-09-23 20:16:18 +0300

    Finished basic introduction

--- src/Components/WelcomeSection.jsx
+++ src/Components/WelcomeSection.jsx
@@ -2,9 +2,11 @@
 function Welcome(){
     return (
         <section id="welcome-section">
-            <h1 id="developer">Hey, I am Guy.</h1>
-            <span id="developer-title">front-end-developer.</span>
-            <p>I'm currently a high school student, studying physics and system engineering. I try to focus on learning front end as much as I can and do my best.</p>
+            <div id="introduction">
+                <h1 id="developer">Hey, I am Guy.</h1>
+                <span id="developer-title">front-end-developer.</span>
+                <p>I'm currently a high school student, studying <strong>physics</strong> and <strong>system engineering</strong>. I try to focus on learning front end as much as I can and do my best.</p>
+            </div>
         </section>
     );
 }
